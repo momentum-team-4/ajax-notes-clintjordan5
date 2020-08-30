da530f12d2beaafba953c25a812d1ac55b6a1f6d
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-30 16:38:46 -0400

    fixed js fetch function working on delete function

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const foodLog = document.querySelector("#meals")
 const saveLog = document.querySelector("#save-log")
 const deleteLog = document.querySelector("#delete-log")
-const notesOutput = document.querySelector ("#output")
+const notesOutput = document.querySelector("#output")
 
 
-saveLog.addEventListener ("click", function () {
-    event.preventDefault()
-    let logText = document.querySelector(".meals").value
-    output.textContent= logText + " Meals Logged " ; 
+saveLog.addEventListener("click", function () {
+  event.preventDefault()
+  let logText1 = document.querySelector("#meal1").value
+  let logText2 = document.querySelector("#meal2").value
+  let logText3 = document.querySelector("#meal3").value
+  let logText4 = document.querySelector("#meal4").value
+  let logText5 = document.querySelector("#meal5").value
+  output.textContent = logText1 + logText2 + logText3 + logText4 + logText5 + " Testing Output ";
+  fetch('http://localhost:3000/notes/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ "meal1": logText1})
+  })
+    .then(r => r.json())
+    .then(data => {console.log(data)}
+    )
 })
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
   fetch('http://localhost:3000/notes/')
-  .then(response => response.json())
-  .then(data => {console.log(data)})
+    .then(response => response.json())
+    .then(data => { console.log(data) })
 })
 
 
 fetch('http://localhost:3000/notes/', {
-  method: 'POST', 
-  headers: {"Nutrition Log": "application/json"}, 
-  body: JSON.stringify({"title": "test", "test 2": "test 3"})
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ meals: meal1.value, datetime: moment().format('YYYY-MM-DD HH:mm') })
 })
-.then(r => r.json())
-.then(
-)
+  .then(r => r.json())
+  .then(newMood => {
+    const newMoodEl = createMoodEl(newMood)
+    addToMoodsDiv(newMoodEl)
+  })
+})
