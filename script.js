27e2bb19cef740adc4b99cfab76cd229a80e519c
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-31 14:59:30 -0400

    updated html/css/java with notes

--- script.js
+++ script.js
@@ -5,25 +5,23 @@ const saveLog = document.querySelector("#save-log")
 const deleteLog = document.querySelector("#delete-log")
 const notesOutput = document.querySelector("#output")
 
+// constants referencing html ID
 
 saveLog.addEventListener("click", function () {
   event.preventDefault()
-  let logText1 = document.querySelector("#meal1").value
-  let logText2 = document.querySelector("#meal2").value
-  let logText3 = document.querySelector("#meal3").value
-  let logText4 = document.querySelector("#meal4").value
-  let logText5 = document.querySelector("#meal5").value
-  output.textContent = logText1 + logText2 + logText3 + logText4 + logText5;
+  let logText1 = document.querySelector("#PCF").value
+  output.textContent = logText1;
   fetch('http://localhost:3000/notes/', {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ "meal1": logText1 })
-    // where to add meal 2, meal 3, etc?
+    body: JSON.stringify({ "PCF": logText1 })
   })
     .then(r => r.json())
     .then(data => { console.log(data) }
     )
 })
+// this works in concept but the goal is not to print to a premade output in html, 
+// rather make a new element with js
 
 window.addEventListener('load', function () {
   fetch('http://localhost:3000/notes/')
@@ -31,16 +29,20 @@ window.addEventListener('load', function () {
     .then(data => { console.log(data) })
 })
 
+// this is for when the page initially loads
 
-fetch('http://localhost:3000/notes/', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ meals: meal1.value, datetime: moment().format('YYYY-MM-DD HH:mm')})
-})
-  .then(r => r.json())
-  .then(newMeal => {
-    const newMealEl = createMealEl(newMeal)
-    addToMealsDiv(newMealEl)
+notesOutput.addEventListener('submit', function (event) {
+  event.preventDefault()
+  fetch('http://localhost:3000/moods/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ notes: PCF.value, datetime: moment().format('YYYY-MM-DD HH:mm') })
   })
+    .then(r => r.json())
+    .then(newNote => {
+      const newNoteEl = createNoteEl(newNote)
+      addToFoodLog(newNoteEl)
+    })
+})
 
-// need to define moment and newMealEl
+// no console errors, but purpose here is to make a new note element with the value from PCF with a timestamp
